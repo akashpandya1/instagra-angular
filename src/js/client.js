96bc095d8009ec759935ab9193eb8eecab130ca3
akashpandya1 <akashpandya@gmail.com>
2016-09-20 15:06:00 -0400

    made changes for the route

--- src/js/client.js
+++ src/js/client.js
@@ -3,27 +3,40 @@ var app = angular.module('instaApp', ["ngRoute"]);
   
 app.config(['$routeProvider',
   function($routeProvider) {
-     $routeProvider.when('/ShowOrder/:orderId', {
-	templateUrl: 'static/showorders.html',
-	controller: 'ShowOrderController'
-      });
+     $routeProvider
+     .when('/', {
+            templateUrl: 'static/logon.html'           
+          })
+     .when('/ShowFeeds/:userId', {
+            templateUrl: 'static/showFeeds.html',
+            controller: 'instaCtrl'
+      })
+      .otherwise({
+            redirectTo: '/'
+          });
 }]);
  
-app.controller('ShowOrderController', function($scope, $routeParams) {
-	$scope.order_id = $routeParams.orderId;
-});
+ 
 
-app.controller('instaCtrl', function instaCtrl($scope, $http) { 
-        console.log("inside instaCtrl");  
-        $http({
+app.controller('instaCtrl', function instaCtrl($scope, $http, $routeParams) {   
+        console.log("inside instaCtrl:" + $routeParams.userId);     
+         $http({
                 method: 'GET',                
-                url: 'http://localhost:8080/getFollowerPosts/1/'
+                url: 'http://localhost:8080/getFollowerPosts/' + $routeParams.userId + '/'
             }).then(function successCallback(response) {
                 console.log(response.data);               
                 $scope.followerPosts = response.data;                  
             }, function errorCallback(response) {
                 console.log(response);
-            });       
+            }); 
 }); 
+
+
+/**
+ * app.controller('ShowOrderController', function($scope, $routeParams) {
+	$scope.order_id = $routeParams.orderId;
+});
+ * 
+ */
 
  
