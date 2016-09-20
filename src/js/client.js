174dddba7d0d84671e750855ebb3b1a8552ec5eb
akashpandya1 <akashpandya@gmail.com>
2016-09-20 13:22:22 -0400

    added showorders.html

--- src/js/client.js
+++ src/js/client.js
@@ -1,13 +1,25 @@
   
-var app = angular.module('instaApp', []);
+var app = angular.module('instaApp', ["ngRoute"]);
   
+app.config(['$routeProvider',
+  function($routeProvider) {
+     $routeProvider.when('/ShowOrder/:orderId', {
+	templateUrl: 'static/showorders.html',
+	controller: 'ShowOrderController'
+      });
+}]);
+ 
+app.controller('ShowOrderController', function($scope, $routeParams) {
+	$scope.order_id = $routeParams.orderId;
+});
+
 app.controller('instaCtrl', function instaCtrl($scope, $http) { 
         console.log("inside instaCtrl");  
         $http({
                 method: 'GET',                
                 url: 'http://localhost:8080/getFollowerPosts/1/'
             }).then(function successCallback(response) {
-                console.log(response.data);
+                console.log(response.data);               
                 $scope.followerPosts = response.data;                  
             }, function errorCallback(response) {
                 console.log(response);
